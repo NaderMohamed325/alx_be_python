325cab929aed40fa4bb2ce59c124e1755dfe3f3a
Nader Mohamed Nader <nadoorclasher@gmail.com>
2025-06-09 15:19:35 +0300

    Update multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,8 +1,8 @@
 # multiplication_table.py
 
 # Prompt user for a number
-num = int(input("Enter a number to see its multiplication table: "))
+number = int(input("Enter a number to see its multiplication table: "))
 
 # Print multiplication table from 1 to 10
 for i in range(1, 11):
-    print(f"{num} * {i} = {num * i}")
+    print(f"{number} * {i} = {number * i}")
