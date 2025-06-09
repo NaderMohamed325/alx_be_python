b7787b0ca3f76b4294d25ccaab95ec50e612bcf0
Nader Mohamed Nader <nadoorclasher@gmail.com>
2025-06-09 15:16:50 +0300

    Create match_case_calculator.py

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,14 @@
+match operation:
+    case "+":
+        result = num1 + num2
+    case "-":
+        result = num1 - num2
+    case "*":
+        result = num1 * num2
+    case "/":
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            result = num1 / num2
+    case _:
+        print("Invalid operation.")
